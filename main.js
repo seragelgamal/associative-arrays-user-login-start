385669e21e52b0303bd63fc3f0e4255348829400
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2020-07-09 11:55:50 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
 // User Login
 
+let passwords = {
+    seragelgamal: 'elephant candy',
+    bob2005: 'password123',
+    theman: "I'm the man",
+}
+
 // HTML Element
 let loginBtnEl = document.getElementById('login-btn');
+let signUpButton = document.getElementById('signup-btn');
 let usernameInputEl = document.getElementById('username-in');
 let passwordInputEl = document.getElementById('password-in');
 let feedbackEl = document.getElementById('feedback');
 
 // Event Listener
 loginBtnEl.addEventListener('click', login);
+signUpButton.addEventListener('click', signUp);
 
 // Event Function
 function login() {
     // Get User Login Input
-    let username = usernameInputEl.value;
-    let password = passwordInputEl.value;
-
-    // Test User Login Input
-    if (username == 'admin' && password == 'password') {
+    if (passwords[usernameInputEl.value] == undefined) {
+        feedbackEl.innerHTML = 'Invalid Username';
+    } else if (passwords[usernameInputEl.value] == passwordInputEl.value) {
         feedbackEl.innerHTML = 'Login Successful';
     } else {
-        feedbackEl.innerHTML = 'Login Unsuccessful';
+        feedbackEl.innerHTML = 'Invalid Password';
     }
+}
+
+function signUp() {
+    if (passwords[usernameInputEl.value] == undefined) {
+        passwords[usernameInputEl.value] = passwordInputEl.value;
+        feedbackEl.innerHTML = 'Signup Successful';
+    } else {
+        feedbackEl.innerHTML = 'Username Taken';
+    }
+
 }
